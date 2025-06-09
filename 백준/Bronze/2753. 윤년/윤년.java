a2e3239e7b223ecbf3752fc8d5e8af32187d2371
omega0493 <87972082+omega0493@users.noreply.github.com>
2025-06-09 19:00:34 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,19 +1,20 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int A = sc.nextInt();
-		
-		if(A%4==0 && A%100!=0 || A%400==0) {
-			System.out.println("1");
-		} else {
-			System.out.println("0");
-		}
-				
-		sc.close();
-		
-	}
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String readLine = br.readLine();
+
+        int year = Integer.parseInt(readLine);
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
 }
