dced4e97d3e9a64c58e85fa174b2f98324c068a3
omega0493 <87972082+omega0493@users.noreply.github.com>
2025-06-09 17:40:19 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] readLine = br.readLine().split(" ");
+
+        int a = Integer.parseInt(readLine[0]);
+        int b = Integer.parseInt(readLine[1]);
+
+        if (a > b) {
+            System.out.println(">");
+        } else if (a < b) {
+            System.out.println("<");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
