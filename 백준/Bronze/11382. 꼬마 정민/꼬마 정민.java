09a5b669da23a578b1c915baefc4f646d84aca7a
omega0493 <87972082+omega0493@users.noreply.github.com>
2025-06-09 17:12:31 +0900

    [Bronze V] Title: 꼬마 정민, Time: 112 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = bufferedReader.readLine().split(" ");
+
+        long A = Long.parseLong(input[0]);
+        long B = Long.parseLong(input[1]);
+        long C = Long.parseLong(input[2]);
+
+        System.out.println(A + B + C);
+    }
+}
