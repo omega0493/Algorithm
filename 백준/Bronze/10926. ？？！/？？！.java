a64ad7f99fc84a544e0d7ea0ef7ae53458b560f7
omega0493 <87972082+omega0493@users.noreply.github.com>
2025-06-09 16:35:28 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+
+        System.out.println(a + "??!");
+        scanner.close();
+    }
+}
