8ef30c28cc2b674be42e0d7702de39f9174cfac2
omega0493 <87972082+omega0493@users.noreply.github.com>
2025-06-19 01:33:55 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 168 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        int length = s.length();
+
+        System.out.println(length);
+    }
+}
