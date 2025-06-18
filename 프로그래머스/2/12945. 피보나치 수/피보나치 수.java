dececf85eb84773cee428c96522c67a17125d248
omega0493 <87972082+omega0493@users.noreply.github.com>
2025-06-18 21:11:00 +0900

    [level 2] Title: 피보나치 수, Time: 7.65 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,28 @@
+class Solution {
+    
+    static int[] fibo;
+    
+    public int solution(int n) {
+        int answer = 0;
+        
+        fibo = new int[n + 1];
+        
+        int i = DFS(n);
+        
+        answer = fibo[n];
+        
+        return answer;
+    }
+    
+    public int DFS(int n) {
+        if(fibo[n] > 0) {
+            return fibo[n];
+        } else if(n == 0) {
+            return fibo[n] = 0;
+        } else if(n == 1) {
+            return fibo[n] = 1;
+        } else {
+            return fibo[n] = (DFS(n - 2) + DFS(n - 1)) % 1234567;
+        }
+    }
+}
