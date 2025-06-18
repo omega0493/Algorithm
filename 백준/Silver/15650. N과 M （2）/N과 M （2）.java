1afef65ef4598fe6dfdcb213adba201345db13dc
omega0493 <87972082+omega0493@users.noreply.github.com>
2025-06-18 17:26:59 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15808 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        DFS(0, 0);
+
+    }
+
+    public static void DFS(int now, int depth) {
+
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = now + 1; i <= N; i++) {
+                arr[depth] = i;
+                DFS(i, depth + 1);
+        }
+
+    }
+}
