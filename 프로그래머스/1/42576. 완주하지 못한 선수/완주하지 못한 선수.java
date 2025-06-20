332021bbb3e62cfa4e0e0195d0d7dc8e81dcf9e3
omega0493 <87972082+omega0493@users.noreply.github.com>
2025-06-20 15:41:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 98.84 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,6 +1,24 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        for(String c : completion) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+        
+        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue() > 0) {
+                answer = entry.getKey();
+            } 
+        }
+        
         return answer;
     }
 }
